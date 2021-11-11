c9a26ae7d0f80fa90f1f9e18ed471b7f95902620
lucas-paz <lucas.hpaz@outlook.com>
2021-11-10 22:25:26 -0300

    errors middleware

---
+++ backend/src/middlewares/errors.js
@@ -0,0 +1,9 @@
+const handleErrors = (err, _req, res, _next) => {
+  if (err.isJoi) {
+    return res.status(400).json({ message: err.details[0].message });
+  }
+
+  return res.status(err.code).json({ message: err.message });
+};
+
+module.exports = handleErrors;
